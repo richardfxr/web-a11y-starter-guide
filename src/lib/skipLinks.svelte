cdadc847eb234ba9beef386acf26dd18b1349ff1
Richard Fu <richardfxr.personal@gmail.com>
2022-04-23 18:53:45 -0400

    🏷️HTML: added heading and ul to skipLnks

--- src/lib/skipLinks.svelte
+++ src/lib/skipLinks.svelte
@@ -2,8 +2,11 @@
 
 </script>
 
-<section id="skiplinks" class="region">
-
+<section id="skiplinks" class="region" aria-labelledby="skiplinks__h2">
+    <h2 id="skiplinks__h2">Navigation</h2>
+    <ul>
+        <li></li>
+    </ul>
 </section>
 
 <style lang="scss">
