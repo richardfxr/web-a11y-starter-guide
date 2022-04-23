6e9f82a0c236866aa46ef9728d4fd0dfd3df11c0
Richard Fu <richardfxr.personal@gmail.com>
2022-04-23 13:44:01 -0400

    🟠SVLT: added sideBar, Footer, & #main to __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,22 @@
-<slot />
+<script>
+    // imports
+    import SideBar from '$lib/sideBar.svelte';
+    import Footer from '$lib/footer.svelte';
+</script>
+
+<SideBar />
+
+<main id="main">
+    <slot />
+</main>
+
+<Footer />
 
 <style lang="scss">
-    
+    #main {
+        max-width: var(--max-width);
+        width: 100%;
+        padding: var(--pad-edge);
+        margin: 0 auto;
+    }
 </style>
