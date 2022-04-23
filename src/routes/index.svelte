b21ede36ec781648300916dd01943a4faa89e460
Richard Fu <richardfxr.personal@gmail.com>
2022-04-23 17:50:16 -0400

    🟠SVLT: added _pageLayout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,21 @@
-<div class="test">
+<script>
+    import PageLayout from "./_pageLayout.svelte";
+</script>
 
-</div>
+<PageLayout>
+    <h1 slot="pageHeading">TEST</h1>
+
+    <main slot="main">
+        <div class="test">
+
+        </div>
+    </main>
+</PageLayout>
+
 
 <style lang="scss">
     .test {
         height: 50vh;
+        background-color: blue;
     }
 </style>
