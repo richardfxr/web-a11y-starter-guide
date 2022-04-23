cc925a59b0b60f52d26cd547913678b239f3cd63
Richard Fu <richardfxr.personal@gmail.com>
2022-04-23 13:42:42 -0400

    ✨NEW: created new components

---
+++ src/lib/sideBar.svelte
@@ -0,0 +1,26 @@
+<script>
+    // imports
+    import SkipLinks from '$lib/skipLinks.svelte';
+    import Nav from '$lib/nav.svelte';
+</script>
+
+<div id="sidebar">
+    <SkipLinks />
+    <Nav />
+</div>
+
+<style lang="scss">
+    #sidebar {
+        // absolute position
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        
+        width: var(--sidebar-width);
+        margin-left: var(--sidenav-pad-centered);
+
+        background-color: red;
+        height: 100vh;
+    }
+</style>
