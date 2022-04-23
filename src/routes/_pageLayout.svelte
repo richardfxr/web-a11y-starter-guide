299bed945398a5329cced4333278dde3e1413325
Richard Fu <richardfxr.personal@gmail.com>
2022-04-23 17:49:59 -0400

    ✨NEW: replaced __layout with _pageLayout

---
+++ src/routes/_pageLayout.svelte
@@ -0,0 +1,14 @@
+<script>
+    // imports
+    import SideBar from '$lib/sideBar.svelte';
+    import Footer from '$lib/footer.svelte';
+</script>
+
+<slot name="pageHeading"></slot>
+
+<SideBar />
+
+
+<slot name="main"></slot>
+
+<Footer />
