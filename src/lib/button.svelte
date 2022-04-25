a75d2de830196747ffc4bc5bccd27ee725750001
Richard Fu <richardfxr.personal@gmail.com>
2022-04-25 01:32:13 -0400

    ✨NEW: added button component

---
+++ src/lib/button.svelte
@@ -0,0 +1,32 @@
+<script>
+    // === PROPERTIES =============================
+    export let type = "link";
+    export let size = "sm";
+    export let text;
+    export let link = "#";
+    export let target = null;
+</script>
+
+
+{#if type === "link"}
+    <a href={link} class="button link {size}" {target}>{text}</a>
+{/if}
+
+
+<style lang="scss">
+    .button {
+        display: block;
+        text-decoration: none;
+
+        color: var(--clr-900);
+        background-color: var(--clr-0);
+        
+        &.sm {
+            font-weight: 500;
+            font-size: 1.3rem;
+
+            padding: var(--pad-xs) var(--pad-sm);
+            border-radius: var(--bradius-sm);
+        }
+    }
+</style>
