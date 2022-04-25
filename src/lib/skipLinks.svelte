7c3e0a9eaf372123172ea3f5fbdc8c8c416077a2
Richard Fu <richardfxr.personal@gmail.com>
2022-04-25 01:02:36 -0400

    🟠SVLT: added hint to skipLinks

--- src/lib/skipLinks.svelte
+++ src/lib/skipLinks.svelte
@@ -1,9 +1,14 @@
 <script>
-
+    // === IMPORTS ================================
+    import Hint from '$lib/hint.svelte';
 </script>
 
 <section id="skiplinks" class="region" aria-labelledby="skiplinks__h2">
-    <h2 id="skiplinks__h2">Navigation</h2>
+    <div class="heading">
+        <h2 id="skiplinks__h2">Skip Links</h2>
+        <Hint key="S" />
+    </div>
+    
     <ul>
         <li></li>
     </ul>
@@ -13,5 +18,11 @@
     #skiplinks {
         width: 100%;
         height: 25rem;
+
+        .heading {
+            display: flex;
+            flex-flow: row nowrap;
+            gap: var(--gap-xs);
+        }
     }
 </style>
