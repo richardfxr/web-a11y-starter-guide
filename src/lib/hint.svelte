e3b472035d74b54aa31922d6a11d7fd36c71b4db
Richard Fu <richardfxr.personal@gmail.com>
2022-04-25 01:02:14 -0400

    ✨NEW: added hint component

---
+++ src/lib/hint.svelte
@@ -0,0 +1,34 @@
+<script>
+    // === PROPERTIES =============================
+    export let key;
+</script>
+
+
+<kbd class="hint" aria-label="Shortcut key: {key}">{key}</kbd>
+
+
+<style lang="scss">
+    .hint {
+        /* flexbox and centering key */
+        display: flex;
+        align-items: center;
+        padding: 0 var(--pad-xs);
+
+        /* typography */
+        font-family: 'Inter';
+        font-size: 1.2rem;;
+        font-weight: 400;
+
+        /* color and border raidus */
+        color: var(--clr-0);
+        background-color: var(--clr-900);
+        border-radius: var(--bradius-xs);
+    }
+
+    // hints hidden
+    :global([data-hints="hidden"]) {
+        .hint {
+            display: none;
+        }
+    }
+</style>
