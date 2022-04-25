3dc7ad1b30e56538620aae1a7efee81a4259cc1a
Richard Fu <richardfxr.personal@gmail.com>
2022-04-25 01:38:02 -0400

    🟠SVLT: added button and ul to nav

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,14 +1,22 @@
 <script>
-
+    // === IMPORTS ================================
+    import Hint from '$lib/hint.svelte';
+    import Button from '$lib/button.svelte';
 </script>
 
 <nav id="nav" class="region">
-
+    <div class="heading">
+        <h2 id="skiplinks__h2">Navigation</h2>
+        <Hint key="N" />
+    </div>
+    
+    <ul class="options">
+        <li><Button type="link" size="sm" text="Guide" link="#mian" /></li>
+        <li><Button type="link" size="sm" text="Settings" link="#mian" /></li>
+        <li><Button type="link" size="sm" text="About" link="#mian" /></li>
+    </ul>
 </nav>
 
 <style lang="scss">
-    #nav {
-        width: 100%;
-        height: 25rem;
-    }
+
 </style>
