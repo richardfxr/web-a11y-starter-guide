12acf629f396b7b84f75b858271da674b905d1ab
Richard Fu <richardfxr.personal@gmail.com>
2022-04-26 00:15:38 -0400

    ✨NEW: added Svelte stores

---
+++ src/store/store.js
@@ -0,0 +1,36 @@
+import { browser } from '$app/env';
+import { writable } from 'svelte/store';
+
+// initialize stored variables (default values for server)
+let storedSidebarDir = "left";
+let storedSidebarPos = "center";
+
+if (browser) {
+    // get stored values from localStorage if they existt
+    if(localStorage.sidebarDir) storedSidebarDir = localStorage.sidebarDir;
+    if(localStorage.sidebarPos) storedSidebarPos = localStorage.sidebarPos;
+}
+
+/* === STORES ============================= */
+export const curPage = writable(null);
+export const sidebarDir = writable(storedSidebarDir);
+export const sidebarPos = writable(storedSidebarPos);
+
+/* === UPDATES ============================ */
+sidebarDir.subscribe((value) => {
+    // prevent code from running on server
+    if (!browser) return
+
+    // set localStorage and change hasCity if value is truthy
+    localStorage.sidebarDir = value;
+    // update data-sidebar-dir attr
+    document.documentElement.setAttribute("data-sidebar-dir", value);
+});
+
+sidebarPos.subscribe((value) => {
+    // prevent code from running on server
+    if (!browser) return
+
+    // set localStorage and change hasCity if value is truthy
+    localStorage.sidebarPos = value;
+});
