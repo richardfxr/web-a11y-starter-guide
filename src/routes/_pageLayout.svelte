1870235783053984de1279852e5e28da76be20ff
Richard Fu <richardfxr.personal@gmail.com>
2022-04-25 23:10:16 -0400

    🟠SVLT: moved sideBar into _pageLayout

--- src/routes/_pageLayout.svelte
+++ src/routes/_pageLayout.svelte
@@ -1,14 +1,72 @@
 <script>
-    // imports
-    import SideBar from '$lib/sideBar.svelte';
+    // === IMPORTS ================================
+    import Nav from '$lib/nav.svelte';
     import Footer from '$lib/footer.svelte';
 </script>
 
 <slot name="pageHeading"></slot>
 
-<SideBar />
+<div id="sidebar">
+    <slot name="sidebar"></slot>
+    <Nav />
+</div>
 
 
 <slot name="main"></slot>
 
 <Footer />
+
+
+<style lang="scss">
+    #sidebar {
+        /* absolute position */
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        
+        /* sizing */
+        width: var(--sidebar-width);
+        height: 100vh;
+        padding: var(--gap-md) 0;
+
+        /* allow scroll if overflown */
+        overflow: auto;
+
+        /* flexbox */
+        display: flex;
+        flex-flow: column nowrap;
+        gap: var(--gap-md);
+    }
+
+    // left center sidebar
+    :global([data-sidebar-dir="left"][data-sidebar-pos="center"]) {
+        #sidebar {
+            /* assign left margin to center sidebar */
+            margin: 0 auto 0 var(--sidebar-pad-centered);
+        }
+    }
+
+    // right center sidebar
+    :global([data-sidebar-dir="right"][data-sidebar-pos="center"]) {
+        #sidebar {
+            /* assign right margin to center sidebar */
+            margin: 0 var(--sidebar-pad-centered) 0 auto;
+        }
+    }
+
+    // left edge sidebar
+    :global([data-sidebar-dir="left"][data-sidebar-pos="edge"]) {
+        #sidebar {
+            margin: 0 auto 0 var(--gap-md);
+        }
+    }
+
+    // right edge sidebar
+    :global([data-sidebar-dir="right"][data-sidebar-pos="edge"]) {
+        #sidebar {
+            margin: 0 var(--gap-md) 0 auto;
+        }
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,13 @@
 <script>
+    // === IMPORTS ================================
     import PageLayout from "./_pageLayout.svelte";
+    import SkipLinks from '$lib/skipLinks.svelte';
 </script>
 
 <PageLayout>
-    <h1 slot="pageHeading">TEST</h1>
+    <h1 slot="pageHeading">Web Accessibility Starter Guide</h1>
+
+    <SkipLinks slot="sidebar" />
 
     <main slot="main">
         <div class="region">
